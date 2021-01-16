96b709f99dcb67f1aa7c99143abdb8d7af3fbe8b
ttngu <tammyn137@gmail.com>
2021-01-15 19:41:51 -0800

    updated API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/";
-const APIKEY = "";
-
-// Export an object with a "search" method that searches the API for the passed query
 export default {
-  search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
-  }
-};
+    getUsers: function() {
+        return axios.get("https://randomuser.me/api/?results=200&nat=us");
+    }
+}
