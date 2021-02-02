b128bff7c958b4ccfe679d15aa282f5293f5d635
ttngu <tammyn137@gmail.com>
2021-02-01 16:32:32 -0800

    added jumbotron to header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="jumbotron" style={styles.header}>
+            <h1 className="display-2 text-center font-weight-bold">Employee Directory</h1>
+        </div>
+    )
+}
+
+export default Header
