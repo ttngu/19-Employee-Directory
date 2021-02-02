1d7ee949459755e41bdf5f6472f1c8d713429c6d
ttngu <tammyn137@gmail.com>
2021-02-01 17:02:53 -0800

    added classNames and props to th's

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,15 +6,15 @@ function Table() {
             <table className="Table">
                 <thead>
                     <tr>
-                        <th>Picture</th>
-                        <th>Name</th>
-                        <th>Email Address</th>
-                        <th>Phone Number</th>
-                        <th>Location</th>
+                        <th className="text-center" scope="col">Picture</th>
+                        <th className="text-center hover" scope="col" data-value="name" onClick={props.nameSort}>Name</th>
+                        <th className="text-center" scope="col">Email Address</th>
+                        <th className="text-center" scope="col">Phone Number</th>
+                        <th className="text-center hover" scope="col" data-value="state" onClick={props.stateSort}>Location</th>
                     </tr>
                 </thead>
                 <tbody>
-                    
+
                 </tbody>
             </table>
             
