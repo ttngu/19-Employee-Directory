e97447ac68fa77a6fc0fce55baa9c39b9d025408
ttngu <tammyn137@gmail.com>
2021-02-01 16:58:13 -0800

    added table className

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,9 @@ import React from 'react'
 function Table() {
     return (
         <div>
+            <table className="Table">
+                
+            </table>
             
         </div>
     )
