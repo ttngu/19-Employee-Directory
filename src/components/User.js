1afd93919bb0b3cbc35b8d26efb95d31147fd7cc
ttngu <tammyn137@gmail.com>
2021-02-01 17:08:57 -0800

    added code for sort and imported to user

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,8 @@
 import { Dropdown } from 'bootstrap'
 import React from 'react'
+import Table from "./Table";
+import Sort from "./Sort";
+import API from "../utils/API";
 
 function User() {
     return (
